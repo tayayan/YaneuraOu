8d285e4af9aa876739c841cbba6ec37e0571c36f
tayayan <66405032+tayayan@users.noreply.github.com>
2023-08-03 20:18:44 +0900

    doEvenDeeperSearch
    https://github.com/official-stockfish/Stockfish/commit/98965c139df1483a3d684ee8bc7a60dc4b95efa1

--- source/engine/yaneuraou-engine/yaneuraou-search.cpp
+++ source/engine/yaneuraou-engine/yaneuraou-search.cpp
@@ -2584,9 +2584,10 @@ namespace {
 					// Adjust full depth search based on LMR results - if result
 					// was good enough search deeper, if it was bad enough search shallower
 					const bool doDeeperSearch = value > (alpha + 78 + 11 * (newDepth - d));
+					const bool doEvenDeeperSearch = value > alpha + 582;
 					const bool doShallowerSearch = value < bestValue + newDepth;
 					
-					newDepth += doDeeperSearch - doShallowerSearch;
+					newDepth += doDeeperSearch - doShallowerSearch + doEvenDeeperSearch;
 
 					if (newDepth > d)
 						value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, !cutNode);
