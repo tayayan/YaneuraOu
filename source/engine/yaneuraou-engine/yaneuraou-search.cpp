1a3a6cb90d2176d7ee0e6b2f8cf14ae7acc3ce9e
tayayan <66405032+tayayan@users.noreply.github.com>
2023-08-03 19:38:37 +0900

    Remove old line in "Futility pruning for captures"
    https://github.com/official-stockfish/Stockfish/commit/d5271af0ee28cb52e1033f183f3b34754aa5efa0

--- source/engine/yaneuraou-engine/yaneuraou-search.cpp
+++ source/engine/yaneuraou-engine/yaneuraou-search.cpp
@@ -2247,8 +2247,7 @@ namespace {
 #endif
 
 					// Futility pruning for captures (~0 Elo)
-					if (   !pos.empty(to_sq(move))
-						&& !givesCheck
+					if (   !givesCheck
 						&& !PvNode
 						&& lmrDepth < 6
 						&& !ss->inCheck
